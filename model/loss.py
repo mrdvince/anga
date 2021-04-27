b17c96d0cd452f843ef550d605354c9095bd9bde
Vince <vincivenv@gmail.com>
2021-04-27 18:54:50 +0300

    include cross entropy loss

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,9 @@
 import torch.nn.functional as F
 
 
+def cross_entropy(output, target):
+    return F.cross_entropy(output, target)
+
+
 def nll_loss(output, target):
     return F.nll_loss(output, target)
