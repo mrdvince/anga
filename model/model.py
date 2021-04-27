a09ccd3ef50f3d2455e03b7694ee141db358f9a5
Vince <vincivenv@gmail.com>
2021-04-27 18:56:44 +0300

    fix: [no attribute 'parameters']
    rename features to conv_stem

--- model/model.py
+++ model/model.py
@@ -10,7 +10,7 @@ class EffientNetB0(BaseModel):
         self.model = torch.hub.load(
             "rwightman/gen-efficientnet-pytorch", "efficientnet_b0", pretrained=True
         )
-        for param in self.model.features.parameters():
+        for param in self.model.conv_stem.parameters():
             param.requires_grad = False
         self.model.classifier = nn.Sequential(
             nn.Linear(1280, 512), nn.Dropout(0.2), nn.ReLU(), nn.Linear(512, 38)
@@ -26,10 +26,10 @@ class MixNetM(BaseModel):
         self.model = torch.hub.load(
             "rwightman/gen-efficientnet-pytorch", "mixnet_m", pretrained=True
         )
-        for param in self.model.features.parameters():
+        for param in self.model.conv_stem.parameters():
             param.requires_grad = False
         self.model.classifier = nn.Sequential(
-            nn.Linear(1280, 512), nn.Dropout(0.2), nn.ReLU(), nn.Linear(512, 38)
+            nn.Linear(1536, 512), nn.Dropout(0.2), nn.ReLU(), nn.Linear(512, 38)
         )
 
     def forward(self, x):
