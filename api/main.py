bfd752e7cb8a7fc236fbad0e8f0c2482e3d4f62a
mrdvince <vincivenv@gmail.com>
2021-04-29 21:58:24 +0300

    api docs description update

--- api/main.py
+++ api/main.py
@@ -8,10 +8,45 @@ from torchvision import transforms
 import numpy as np
 import os
 import json
+from pydantic import BaseModel
+
 
 app = FastAPI(
     title="Plant leaf disease classifier",
-    description="Given a plant leaf photo, return check the type of disease it has, (limited to the 38 classes trained on). Model used is a pretrained efficientnet b0, mixnet medium is also available",
+    description="""
+Given a plant leaf photo, 
+return check the type of disease it has
+(limited to the 38 classes trained on). 
+Model used is a pretrained efficientnet b0, mixnet medium is also available
+> Note: the dataset has __38 classes__ but some of the classes are healthy plant leafs
+### Disease types
+- Apple Frogeye Spot
+- Apple Scab
+- Cedar Apple Rust
+- Cherry Powdery Mildew
+- Maize Cercospora Leaf Spot
+- Maize Common Rust
+- Maize Northern Leaf Blight
+- Grape Black Rot
+- Grape Esca Black Measles
+- Grape Isariopsis Leaf Spot
+- Orange Haunglongbing
+- Peach Bacterial Spot
+- Pepper Bacterial Spot
+- Potato Early Blight
+- Potato Late Blight
+- Squash Powdery Mildew
+- Strawberry Leaf Scorch
+- Tomato Bacterial Spot
+- Tomato Early Blight
+- Tomato Late Blight
+- Tomato Leaf Mold
+- Tomato Septoria Leaf Spot
+- Tomato Two Spotted Spider Mite
+- Tomato Target Spot
+- Tomato Yellow Leaf Curl Virus
+- Tomato Mosaic Virus
+    """,
     docs_url="/playground",
     redoc_url="/docs",
 )
@@ -53,8 +88,13 @@ def get_classes():
     return cats
 
 
-@app.post("/")
+@app.post("/", tags=["Post image"])
 def get_predictions(file: UploadFile = File(...)):
+    """
+    Upload an image file  and get a prediction/classification (whatever you want to call it)
+
+    - **upload file**: the intended image file
+    """
     pred = predict(file)
     cats = get_classes()
     return {"index": int(pred), "label": cats[str(pred)]}
